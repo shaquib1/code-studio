f74ce0904edbd1f9e4b860dee3d5d74abe272c3c
Md Shaquib Alam <shaquib475@gmail.com>
2023-06-18 17:42:19 +0530

    Print Like a Wave

--- Print_Like_A_Wave.cpp
+++ Print_Like_A_Wave.cpp
@@ -1,29 +1,30 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
 {
-    // Write your code here
+    //Write your code here
 
     vector<int> ans;
 
-    for (int i = 0; i < mCols; i++)
-    {
-        if (i & 1)
-        {
-            for (int j = nRows - 1; j >= 0; j--)
-            {
-                // cout<<arr[j][i]<<" ";
-                ans.push_back(arr[j][i]);
+    
+
+    for(int i=0; i<mCols; i++){
+        if(i==0||i%2==0){
+          
+             for(int j=0; j<nRows; j++){
+              // cout<<arr[j][i]<<" ";
+               ans.push_back(arr[j][i]);
+           }
+
+
+        }else{
+           for(int j=nRows-1; j>=0; j--){
+               // cout<<arr[j][i]<<" ";
+                 ans.push_back(arr[j][i]);
             }
-        }
-        else
-        {
-            for (int j = 0; j < nRows; j++)
-            {
-                // cout<<arr[j][i]<<" ";
-                ans.push_back(arr[j][i]);
-            }
+
         }
     }
-
+    
     return ans;
+
 }
